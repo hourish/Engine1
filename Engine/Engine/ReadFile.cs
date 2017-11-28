ffa335d5ebb8bb0a69c11e6204fc2a1a6b9bdde3
Shani <hourish@post.bgu.ac.il>
2017-11-28 13:43:28 +0200

    commit 13:43

--- Engine/Engine/ReadFile.cs
+++ Engine/Engine/ReadFile.cs
@@ -19,18 +19,29 @@ namespace Engine
 
         public void Seperate()
         {
-            List<string> docs = new List<string>();
+            List<string> docTEXT = new List<string>();
             List<string> docNum = new List<string>();
 
             string[] filesPaths = System.IO.Directory.GetFiles(path, "*.*", System.IO.SearchOption.AllDirectories);
             string[] fileText = new string[filesPaths.Length];
-            Regex r = new Regex(Regex.Escape("<DOCNO>") + "(.*?)" + Regex.Escape("</DOCNO>"));
+            Regex rDOCNO = new Regex(Regex.Escape("<DOCNO>") + "(.*?)" + Regex.Escape("</DOCNO>"));
+            Regex rTEXT = new Regex(Regex.Escape("<TEXT>") + "(.*?)" + Regex.Escape("</TEXT>"));
             for (int i = 0; i < filesPaths.Length; i++)
             {
                 fileText[i] = File.ReadAllText(filesPaths[i]);
-                MatchCollection matches = r.Matches(fileText[i]);
-                foreach (Match match in matches)
-                    docNum.Add(match.Groups[1].Value);
+                MatchCollection matchesDOCNO = rDOCNO.Matches(fileText[i]);
+                MatchCollection matchesTEXT = rTEXT.Matches(fileText[i]);
+               // foreach (Match match in matchesDOCNO)
+               //     docNum.Add(match.Groups[1].Value);
+                foreach (Match match in matchesTEXT)
+                    docTEXT.Add(match.Groups[1].Value);
+
+                Match matchNo = rDOCNO.Match(fileText[i]);
+                while(matchNo.Success)
+                {
+                    docNum.Add(matchNo.Groups[1].Value);
+                    matchNo = matchNo.NextMatch();
+                }
             }
             //    string del = @"\<DOCNO\>(.*?)\<\/TEXT\>";
             //string del = @"\b\<DOCNO\>+\s\<\/TEXT\>+\b"
