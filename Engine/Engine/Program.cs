b66b1f581111f210e05575ecf5f1701482aa0c76
Shani <hourish@post.bgu.ac.il>
2017-11-28 13:20:43 +0200

    commit

--- Engine/Engine/Program.cs
+++ Engine/Engine/Program.cs
@@ -10,7 +10,7 @@ namespace Engine
     {
         static void Main(string[] args)
         {
-            ReadFile rf = new ReadFile(@"C:\Users\Shani\Desktop\שנה ג\סמסטר א\אחזור מידע\מנוע\dugma");
+            ReadFile rf = new ReadFile(@"C:\Users\Shani\Desktop\study\first semester\Ihzur\Engine\dugma");
             rf.Seperate();
         }
     }
--- Engine/Engine/ReadFile.cs
+++ Engine/Engine/ReadFile.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
+using System.Collections;
+using System.Text.RegularExpressions;
 
 namespace Engine
 {
@@ -16,7 +19,26 @@ namespace Engine
 
         public void Seperate()
         {
-            Console.WriteLine(path);
+            List<string> docs = new List<string>();
+            List<string> docNum = new List<string>();
+
+            string[] filesPaths = System.IO.Directory.GetFiles(path, "*.*", System.IO.SearchOption.AllDirectories);
+            string[] fileText = new string[filesPaths.Length];
+            Regex r = new Regex(Regex.Escape("<DOCNO>") + "(.*?)" + Regex.Escape("</DOCNO>"));
+            for (int i = 0; i < filesPaths.Length; i++)
+            {
+                fileText[i] = File.ReadAllText(filesPaths[i]);
+                MatchCollection matches = r.Matches(fileText[i]);
+                foreach (Match match in matches)
+                    docNum.Add(match.Groups[1].Value);
+            }
+            //    string del = @"\<DOCNO\>(.*?)\<\/TEXT\>";
+            //string del = @"\b\<DOCNO\>+\s\<\/TEXT\>+\b";
+            //string del = Regex.Escape("<DOCNO>") + "(.*?)" + Regex.Escape("</TEXT>");
+          /*  string docNo = "<DOCNO>(.*?)</DOCNO>";
+            string doc = "<DOC>(.*?)</DOC>";
+            docNum.AddRange(Regex.Split(fileText[0], docNo));
+            docs.AddRange(Regex.Split(fileText[0], doc));*/
         }
     }
 }
